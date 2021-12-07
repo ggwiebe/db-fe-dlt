dcc321119d2d7c070bafbe8b0fe3f040801cc473
Glenn Wiebe <Glenn.Wiebe@databricks.com>
2021-12-07 17:52:26 -0500

    Add more Loan Queries

--- dlt/scripts/Loans-Queries.sql
+++ dlt/scripts/Loans-Queries.sql
@@ -14,6 +14,30 @@ SELECT COUNT(*)
 
 -- COMMAND ----------
 
+-- Get details of raw loan stats
+SELECT *
+  FROM ggw_loans.bz_reference_loan_stats
+;
+
+-- COMMAND ----------
+
+-- Get details of loan stats
+SELECT a.*
+  FROM      ggw_loans.BZ_reference_loan_stats a
+ INNER JOIN ggw_loans.ref_accounting_treatment b 
+            USING (id)
+-- COMMAND ----------
+
+-- Get details for silver with matching Account Treatment ID
+  SELECT txs.*, 
+        rat.id AS accounting_treatment 
+   FROM ggw_loans.BZ_raw_txs txs
+  INNER JOIN ggw_loans.ref_accounting_treatment rat 
+          ON txs.accounting_treatment_id = rat.id
+;
+
+-- COMMAND ----------
+
 -- Get details from Gold Load Balance Table #1
 SELECT location_code,
        bal
@@ -29,3 +53,11 @@ SELECT location_code,
   FROM ggw_loans.gl_total_loan_balances_2
  ORDER BY bal DESC
 ;
+
+-- COMMAND ----------
+
+-- Get loan stats for only those with matching accounting treatment id
+SELECT a.*
+  FROM      ggw_loans.bz_reference_loan_stats a
+ INNER JOIN ggw_loans.ref_accounting_treatment b 
+            USING (id)

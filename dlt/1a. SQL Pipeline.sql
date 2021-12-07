a897faa75efd5d0f9303fe4f08afa0085fd31f9c
ggwiebe <ggwiebe>
2021-12-07 21:59:00 +0000

    Rename for dual SQL & Python notebooks

--- dlt/1a. SQL Pipeline.sql
+++ dlt/1a. SQL Pipeline.sql
@@ -2,13 +2,17 @@
 CREATE INCREMENTAL LIVE TABLE BZ_raw_txs
 COMMENT "New raw loan data incrementally ingested from cloud object storage landing zone"
 TBLPROPERTIES ("quality" = "bronze")
-AS SELECT * FROM cloud_files('/Users/glenn.wiebe@databricks.com/dlt_demo/landing', 'json')
+AS 
+SELECT *
+  FROM cloud_files('/Users/glenn.wiebe@databricks.com/dlt_demo/landing', 'json')
 
 -- COMMAND ----------
 
 CREATE LIVE TABLE ref_accounting_treatment
 COMMENT "Lookup mapping for accounting codes"
-AS SELECT * FROM delta.`/Users/glenn.wiebe@databricks.com/dlt_demo/ref_accounting_treatment/`
+AS 
+SELECT *
+  FROM delta.`/Users/glenn.wiebe@databricks.com/dlt_demo/ref_accounting_treatment/`
 
 -- COMMAND ----------
 
@@ -31,34 +35,57 @@ TBLPROPERTIES ("quality" = "silver") AS
 CREATE INCREMENTAL LIVE TABLE BZ_reference_loan_stats
 COMMENT "Raw historical transactions"
 TBLPROPERTIES ("quality" = "bronze")
-AS SELECT * FROM cloud_files('/databricks-datasets/lending-club-loan-stats/LoanStats_*', 'csv')
+AS 
+SELECT *
+  FROM cloud_files('/databricks-datasets/lending-club-loan-stats/LoanStats_*', 'csv')
 
 -- COMMAND ----------
 
 CREATE LIVE TABLE SV_historical_txs
 COMMENT "Historical loan transactions"
 TBLPROPERTIES ("quality" = "silver")
-AS SELECT a.* FROM LIVE.BZ_reference_loan_stats a
-INNER JOIN LIVE.ref_accounting_treatment b USING (id)
+AS 
+SELECT a.*
+  FROM      LIVE.BZ_reference_loan_stats a
+ INNER JOIN LIVE.ref_accounting_treatment b 
+            USING (id)
 
 -- COMMAND ----------
 
-CREATE LIVE TABLE GL_total_loan_balances_1
-COMMENT "Combines historical and new loan data for unified rollup of loan balances"
-TBLPROPERTIES (
-  "quality" = "gold",
-  "pipelines.autoOptimize.zOrderCols" = "location_code"
-)
-AS SELECT sum(revol_bal)  AS bal, addr_state   AS location_code FROM live.SV_historical_txs  GROUP BY addr_state
-UNION SELECT sum(balance) AS bal, country_code AS location_code FROM live.SV_cleaned_new_txs GROUP BY country_code
-
--- COMMAND ----------
-
-CREATE LIVE TABLE GL_total_loan_balances_2
+CREATE LIVE TABLE GL_Total_Loan_Balances
 COMMENT "Combines historical and new loan data for unified rollup of loan balances"
 TBLPROPERTIES ("quality" = "gold")
-AS SELECT sum(revol_bal)  AS bal, addr_state   AS location_code FROM live.SV_historical_txs  GROUP BY addr_state
-UNION SELECT sum(balance) AS bal, country_code AS location_code FROM live.SV_cleaned_new_txs GROUP BY country_code
+AS 
+SELECT sum(revol_bal) AS bal,
+       addr_state     AS location_code 
+  FROM LIVE.SV_historical_txs
+ GROUP BY addr_state
+UNION 
+SELECT sum(balance)   AS bal,
+       country_code   AS location_code
+  FROM LIVE.SV_cleaned_new_txs
+ GROUP BY country_code
+
+-- COMMAND ----------
+
+-- Moved this to a Python Notebook!
+
+-- CREATE LIVE TABLE GL_Total_Loan_Balances_z
+-- COMMENT "Combines historical and new loan data for unified rollup of loan balances"
+-- TBLPROPERTIES (
+--   "quality" = "gold",
+--   "pipelines.autoOptimize.zOrderCols" = "location_code"
+-- )
+-- AS 
+-- SELECT sum(revol_bal)  AS bal,
+--        addr_state      AS location_code
+--   FROM LIVE.SV_historical_txs
+--  GROUP BY addr_state
+-- UNION
+-- SELECT sum(balance)    AS bal,
+--        country_code    AS location_code
+--   FROM LIVE.SV_cleaned_new_txs
+--  GROUP BY country_code
 
 -- COMMAND ----------
 
@@ -68,9 +95,11 @@ TBLPROPERTIES (
   "quality" = "gold",
   "pipelines.autoOptimize.zOrderCols" = "cost_center_code"
 )
-AS SELECT sum(balance), cost_center_code
-FROM live.SV_cleaned_new_txs
-GROUP BY cost_center_code
+AS 
+SELECT cost_center_code,
+       sum(balance) TotalBalance 
+  FROM live.SV_cleaned_new_txs
+ GROUP BY cost_center_code
 
 -- COMMAND ----------
 
@@ -80,6 +109,7 @@ TBLPROPERTIES (
   "quality" = "gold",
   "pipelines.autoOptimize.zOrderCols" = "country_code"
 )
-AS SELECT sum(count), country_code
-FROM live.SV_cleaned_new_txs
-GROUP BY country_code
+AS 
+SELECT sum(count), country_code
+  FROM live.SV_cleaned_new_txs
+ GROUP BY country_code

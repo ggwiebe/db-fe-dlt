bdbbf461572d8bcef7d95d11aedf3cd972c6b42b
ggwiebe@gmail.com <ggwiebe@gmail.com>
2022-04-22 14:48:00 +0000

    Add DLT for Row-based and Set-based expectations

--- dlt/Quality/row_and_set_dlt.sql
+++ dlt/Quality/row_and_set_dlt.sql
@@ -3,4 +3,46 @@
 
 -- COMMAND ----------
 
+-- Not UC compatible, so neither of these (nor cat.db.table syntax) work:
+-- spark.databricks.sql.initial.catalog.name hive_metastore
+-- USE CATALOG ggw;
 
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE VIEW employee_row_bronze (
+--   CONSTRAINT valid_hire_date EXPECT (id IS NULL)
+)
+COMMENT "The customers buying finished products ingested in incremental with Auto Loader"
+TBLPROPERTIES ("quality" = "bronze")
+AS 
+  SELECT *,
+         now() AS ingest_ts,
+         'Workspace/Repos/glenn.wiebe@databricks.com/db-fe-dlt/dlt/Quality/row_and_set_dlt' AS ingest_src
+    FROM STREAM(ggw_quality.employee_source);
+
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE VIEW employee_dept_set_bronze (
+--   CONSTRAINT no_multiple_managers EXPECT (count <= 1)
+)
+COMMENT "The customers buying finished products ingested in incremental with Auto Loader"
+TBLPROPERTIES ("quality" = "bronze")
+AS 
+  SELECT department,
+         count(*) AS ManagerCount
+    FROM ggw_quality.employee_source
+   GROUP BY department;
+
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE TABLE employee_dept_mgrcnt_bronze (
+  CONSTRAINT valid_hire_date EXPECT (id IS NULL),
+  CONSTRAINT no_multiple_managers EXPECT (count <= 1)
+)
+COMMENT "The customers buying finished products ingested in incremental with Auto Loader"
+TBLPROPERTIES ("quality" = "bronze")
+AS 
+  SELECT *
+    FROM LIVE.employee_row_bronze e,
+    INNER JOIN LIVE.employee_dept_set_bronze d
+      ON e.department = d.department
